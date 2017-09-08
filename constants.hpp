ce57410e84dd140ca06165c34b5977309c47dd62
Aditya Limaye <limaye@deshawresearch.com>
2017-09-08 14:17:29 -0400

    made constants match with values from Dill paper

--- constants.hpp
+++ constants.hpp
@@ -1,6 +1,6 @@
 namespace constants {
-	const double alpha = 5.0;
+	const double alpha = 1000.0;
 	const double beta = 1.0;
-	const double delta = 1.0;
-	const double hyd = 1.0;
+	const double delta = 0.1;
+	const double hyd = 0.1;
 }
